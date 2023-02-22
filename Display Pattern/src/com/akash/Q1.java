d77d076cf796032cef4013822d8dca423efb2140
Akash0772 <akashchaurasia617@gmail.com>
2023-02-22 14:47:21 +0530

    Star Print

---
+++ Display Pattern/src/com/akash/Q1.java
@@ -0,0 +1,21 @@
+package com.akash;
+/*
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+*/
+public class Q1 {
+	public static void main(String[] args) {
+		int m = 5;
+		int n = 5;
+		
+		for(int i = 1; i <= m; i++) {
+			for(int j = 1; j <= n; j++) {
+				System.out.print("*" + " ");
+			}
+			System.out.println();
+		}
+	}
+}
