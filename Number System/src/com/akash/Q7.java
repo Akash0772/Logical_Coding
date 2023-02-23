293e2ba969fcffc9b190598b384c5bbffd219dcf
Akash0772 <akashchaurasia617@gmail.com>
2023-02-23 17:54:42 +0530

    Print Even & Odd

---
+++ Number System/src/com/akash/Q7.java
@@ -0,0 +1,17 @@
+package com.akash;
+//Q7. Find out if a given number is an even or odd?
+import java.util.Scanner;
+public class Q7 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number");
+		int a = sc.nextInt();
+		
+		//Condition
+		if((a % 2) == 0) {
+			System.out.println("even");
+		}else {
+			System.out.println("odd");
+		}
+	}
+}
