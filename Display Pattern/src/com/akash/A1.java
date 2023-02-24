1d27b81a209442d01c1499bd6d95fc6b02b0a351
Akash0772 <akashchaurasia617@gmail.com>
2023-02-24 09:36:52 +0530

    Video Number Pattern

---
+++ Display Pattern/src/com/akash/A1.java
@@ -0,0 +1,24 @@
+package com.akash;
+/*
+1111111111
+1111111111
+1111111111
+1111111111
+1111111111
+1111111111
+1111111111
+1111111111
+1111111111
+1111111111
+ */
+public class A1 {
+	public static void main(String[] args) {
+		for(int i = 1; i <= 10; i++) {
+			for(int j = 1; j <= 10; j++) {
+				System.out.print(1);
+//				System.out.println("1");
+			}
+			System.out.println();
+		}
+	}
+}
---
+++ Display Pattern/src/com/akash/A2.java
@@ -0,0 +1,24 @@
+package com.akash;
+/*
+1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 
+ */
+public class A2 {
+	public static void main(String[] args) {
+		for(int i = 1; i <= 10; i++) {
+			for(int j = 1; j <= 10; j++) {
+				System.out.print(1 + " ");
+//				System.out.println("1");
+			}
+			System.out.println();
+		}
+	}
+}
