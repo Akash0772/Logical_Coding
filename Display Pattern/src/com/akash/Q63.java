75530fe7cdcd4f46059d90b4708a64a2a6a77405
Akash0772 <akashchaurasia617@gmail.com>
2023-02-27 00:49:12 +0530

    Print Char Pattern

---
+++ Display Pattern/src/com/akash/Q63.java
@@ -0,0 +1,37 @@
+package com.akash;
+/*
+      3 
+    3 2 
+  3 2 1 
+3 2 1 0 
+  3 2 1 
+    3 2 
+      3  
+ */
+public class Q63 {
+	public static void main(String[] args) {
+		int n = 3;
+		for(int i = n; i >= 1; i--) {
+			char ch = 'D';
+			for(int j = n; j >= n - i + 1; j--) {
+				System.out.print("  ");
+			}
+			for(int j = n; j >= i; j--) {
+				System.out.print(ch + " ");
+				ch--;
+			}
+			System.out.println();
+		}
+		for(int i = 0; i <= n; i++) {
+			char ch = 'D';
+			for(int j = n+1; j > n - i + 1; j--) {
+				System.out.print("  ");
+			}
+			for(int j = n; j >= i; j--) {
+				System.out.print(ch + " ");
+				ch--;
+			}
+			System.out.println();
+		}
+	}
+}
