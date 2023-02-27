52c4c2d07b64378d7098246b88a4580aeb590066
Akash0772 <akashchaurasia617@gmail.com>
2023-02-27 11:38:18 +0530

    Print Char Pattern

---
+++ Display Pattern/src/com/akash/Q72.java
@@ -0,0 +1,22 @@
+package com.akash;
+/*
+E E E E E 
+ D D D D 
+  C C C 
+   B B 
+    A   
+ */
+public class Q72 {
+	public static void main(String[] args) {
+		int n = 5;
+		for(int i = n; i >= 1; i--) {
+			for(int j = 1; j <= n - i; j++) {
+				System.out.print(" ");
+			}
+			for(int j = 1; j <= i; j++) {
+				System.out.print((char) (i + 64) + " ");
+			}
+			System.out.println();
+		}
+	}
+}
