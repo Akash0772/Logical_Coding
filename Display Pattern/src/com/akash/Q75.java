8c01f377618330880a25276fa9537ddabf29c9a1
Akash0772 <akashchaurasia617@gmail.com>
2023-02-27 11:51:04 +0530

    Print Star Pattern

---
+++ Display Pattern/src/com/akash/Q75.java
@@ -0,0 +1,36 @@
+package com.akash;
+/*
+    * 
+   * * 
+  * * * 
+ * * * * 
+* * * * * 
+ * * * * 
+  * * * 
+   * * 
+    *
+ */
+public class Q75 {
+	public static void main(String[] args) {
+		int n = 5; 
+		for(int i = 1; i <= n; i++) {
+			for(int j = 1; j <= n - i; j++) {
+				System.out.print(" ");
+			}
+			for(int j = 1; j <= i; j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+		
+		for(int i = n - 1; i >= 1; i--) {
+			for(int j = 1; j <= n - i; j++) {
+				System.out.print(" ");
+			}
+			for(int j = 1; j <= i; j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+}
