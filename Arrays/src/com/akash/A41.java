ba0a980bc17c3ae2085f8c7fe7a727e432cba695
Akash0772 <akashchaurasia617@gmail.com>
2023-03-06 19:39:59 +0530

    right rotate by one

---
+++ Arrays/src/com/akash/A41.java
@@ -0,0 +1,34 @@
+package com.akash;
+/*
+Print right rotate by one
+initial content
+10,40,4,50,125,5,800,6,
+final content
+6,10,40,4,50,125,5,800,
+
+
+ */
+public class A41 {
+	public static void main(String[] args) {
+//	  initialization in arrays
+		int[] elements = {10, 40, 4, 50, 125, 5, 800, 6};
+//		index By		  0   1    2   3   4  5
+		System.out.println("initial content");
+		for(int i = 0; i < elements.length; i++) {
+			System.out.print(elements[i] + ",");
+		}
+		System.out.println();
+// 		right rotate logic
+		int temp = elements[elements.length - 1];
+		for(int i = elements.length - 1; i > 0; i--) {
+			elements[i] = elements[i - 1];
+		}
+		elements[0] = temp;
+//		elements[elements.length - 1] = temp;
+		
+		System.out.println("final content");
+		for(int i = 0; i < elements.length; i++) {
+			System.out.print(elements[i] + ",");
+		}
+	}
+}
