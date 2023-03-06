b66d4b73ba2f23b440161d1c49264621c0fa5412
Akash0772 <akashchaurasia617@gmail.com>
2023-03-06 19:38:40 +0530

    left shift by one

---
+++ Arrays/src/com/akash/A35.java
@@ -0,0 +1,31 @@
+package com.akash;
+/*
+Print left shift by one
+initial content
+10,40,4,50,125,5,800,6,
+final content
+40,4,50,125,5,800,6,6,
+
+
+ */
+public class A35 {
+	public static void main(String[] args) {
+//	  initialization in arrays
+		int[] elements = {10, 40, 4, 50, 125, 5, 800, 6};
+//		index By		  0   1    2   3   4  5
+		System.out.println("initial content");
+		for(int i = 0; i < elements.length; i++) {
+			System.out.print(elements[i] + ",");
+		}
+		System.out.println();
+//		left shift by one
+		for(int i = 0; i < elements.length - 1; i++) {
+			elements[i] = elements[i + 1];
+		}
+		
+		System.out.println("final content");
+		for(int i = 0; i < elements.length; i++) {
+			System.out.print(elements[i] + ",");
+		}
+	}
+}
