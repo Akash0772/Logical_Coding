159b65192b8ed1bcd65ee29ccb14361fab4cd84e
Akash0772 <akashchaurasia617@gmail.com>
2023-03-08 01:06:14 +0530

    Odd Index Average Value

---
+++ Arrays/src/com/akash/Q12.java
@@ -0,0 +1,26 @@
+package com.akash;
+import java.util.Scanner;
+// Q12. Find out the average value from all odd indexed elements from a given int array ?
+public class Q12 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter array size");
+		int size = sc.nextInt();
+		System.out.println("Enter arrays element");
+		int arr[] = new int[size];
+		for(int i = 0; i < size; i++) {
+			arr[i] = sc.nextInt();
+		}
+		int sum  = 0;
+		int count = 0;
+		for(int i = 0; i < size; i++) {
+			if(i % 2 != 0) {
+				count++;
+				sum += arr[i];
+			}
+		}
+//		System.out.println(count);
+		double avg = sum / count;
+		System.out.println("Odd index average value of array element " + avg);
+	}
+}
