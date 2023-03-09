f5d7e428ab14fc24ce6f6b8fdf772c8cea7eccee
Akash0772 <akashchaurasia617@gmail.com>
2023-03-09 23:12:43 +0530

    Update Q2.java

--- Number System/src/com/akash/Q2.java
+++ Number System/src/com/akash/Q2.java
@@ -1,5 +1,13 @@
 package com.akash;
 // Q2. Print a smaller number from 3 given numbers.
+/*
+Output
+Enter three numbers
+5
+6
+4
+A smaller Number is 4
+ */
 import java.util.Scanner;
 public class Q2 {
 	public static void main(String[] args) {
@@ -10,9 +18,9 @@ public class Q2 {
 		int num3 = sc.nextInt();
 		
 		//condition 
-		if(num1 <= num2 && num1 <= num3) {
+		if(num1 < num2 && num1 < num3) {
 			System.out.println("A Smaller Number is " + num1);
-		}else if(num2 <= num3 && num2 <= num1){
+		}else if(num2 < num3 && num2 < num1){
 			System.out.println("A Smaller Number is " + num2);
 		}else {
 			System.out.println("A smaller Number is " + num3);
