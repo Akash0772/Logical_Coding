1713765abe556ad1052af3a25b6ec4b3248e174a
Akash0772 <akashchaurasia617@gmail.com>
2023-03-09 20:48:19 +0530

    Swap Reverse Last Half Element

---
+++ Arrays/src/com/akash/Q32.java
@@ -0,0 +1,39 @@
+package com.akash;
+import java.util.Scanner;
+// Q32. Reverse only last half of the elements of given array?
+public class Q32 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter arrays size");
+		int size = 0;
+		if(sc.hasNext()) {
+			size = sc.nextInt();
+		}
+		System.out.println("Enter array elemement");
+		int arr[] = new int[size];
+		for(int i = 0; i < size; i++) {
+			arr[i] = sc.nextInt();
+		}
+		System.out.println("Initial array element ");
+		for(int elements: arr) {
+			System.out.print(elements + ", ");
+		}
+		System.out.println();
+		System.out.println("Reverse final array element");
+		int first = 0;
+		int last = arr.length - 1;
+//		Reverse swap logic
+		while(first < last) {
+			int temp = arr[first];
+			arr[first] = arr[last];
+			arr[last] = temp;
+			
+			first++;
+			last--;
+		}
+//		Last Half
+		for(int i = 0; i < arr.length / 2; i++) {
+			System.out.print(arr[i] + ", ");
+		}
+	}
+}
